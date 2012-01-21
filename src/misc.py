3eafe0d810e5a626c9f977b85a3759bab18afc2b
Trevor <trevp@trevp.net>
2012-01-20 16:07:36 -0800

    Oops adding misc.py.

---
+++ src/misc.py
@@ -0,0 +1,23 @@
+
+from compat import *
+
+################ MISC ###
+
+import sys
+
+def printError(s):
+    """Print error message and exit"""
+    sys.stderr.write("ERROR: %s\n" % s)
+    sys.exit(-1)
+
+# Helper function used by structures to print out their binary elements
+def writeBytes(b):
+    """Write hex-encoded byte array with 16 bytes (32 chars) per line"""
+    s = b2a_hex(b)
+    retVal = ""
+    while s:
+        retVal += s[:32]
+        s = s[32:]
+        if len(s):
+            retVal += "\n                   "
+    return retVal
